3e6c91cdd7084ad9e226ee9eef0b813b2c5ef403
Hyeon818 <142474373+Hyeon818@users.noreply.github.com>
2024-08-07 02:11:51 +0900

    DB server PHP Update

--- htdocs/includes/db.php
+++ htdocs/includes/db.php
@@ -1,11 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "root"; // MySQL 기본 사용자
-$password = "1111"; // MySQL 기본 비밀번호
-$dbname = "test1"; // 데이터베이스 이름
+$servername = "hojung2009.iptime.org";
+$username = "grosql"; // MySQL 기본 사용자
+$password = "!ssp123"; // MySQL 기본 비밀번호
+$dbname = "groesenbahn"; // 데이터베이스 이름
+$port = 43306; // 포트번호
 
 // 데이터베이스 연결 생성
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // 연결 확인
 if ($conn->connect_error) {
